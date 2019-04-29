af402ff893631c75df8b2db477929f0f67a9ac11
Allex <allexwhite@mail.ru>
2019-04-29 19:38:21 +0300

    Убрать ресайз окна о программе

--- AutomationStructure/Automation/Automation/View/About.Designer.cs
+++ AutomationStructure/Automation/Automation/View/About.Designer.cs
@@ -36,6 +36,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(541, 309);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "About";
